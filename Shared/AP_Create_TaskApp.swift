e6dac7c6ea99902976785eff7985beb5d004d823
Sofia Andrade <sandrade@lcs.on.ca>
2022-03-25 11:12:35 -0400

    Initial Commit

---
+++ Shared/AP_Create_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  AP_Create_TaskApp.swift
+//  Shared
+//
+//  Created by Sofia Andrade on 2022-03-25.
+//
+
+import SwiftUI
+
+@main
+struct AP_Create_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
