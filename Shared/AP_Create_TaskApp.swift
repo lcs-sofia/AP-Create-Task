ec407fac320705b2aaa624e6a5eeb546587d76af
Sofia Andrade <sandrade@lcs.on.ca>
2022-03-28 14:35:34 -0400

    added a scroll and navigation view.

--- Shared/AP_Create_TaskApp.swift
+++ Shared/AP_Create_TaskApp.swift
@@ -11,16 +11,7 @@ import SwiftUI
 struct AP_Create_TaskApp: App {
     var body: some Scene {
         WindowGroup {
-           //          ContentView()
-            
-            NavigationView {
-       //         Information(store: store)
-            }
-            .tabItem {
-                Image(systemName: "list.bullet")
-                Text("Grove Time")
-            }
-            
+            InformationDetail()
         }
     }
 }
---
+++ Views/InformationDetail.swift
@@ -0,0 +1,28 @@
+//
+//  InformationDetail.swift
+//  AP Create Task (iOS)
+//
+//  Created by Sofia Andrade on 2022-03-28.
+//
+
+import SwiftUI
+
+struct InformationDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            
+        }
+    }
+}
+
+struct InformationDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            
+       
+        InformationDetail()
+        
+      }
+   }
+}
